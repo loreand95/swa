5e36e90ad2bf12797b643ead51aa739612d66cae
AndreCant <32133805+AndreCant@users.noreply.github.com>
2018-12-20 21:53:44 +0100

    Info e Contatti aziende completo

--- swa_client/js/scripts.js
+++ swa_client/js/scripts.js
@@ -1,9 +1,11 @@
 $('#info1').hide();
 $('#info2').hide();
 
-//Richiesta al server
+
 var myURL = 'http://localhost:8080/swa/rest/aziende';
-var urls = [];
+var uri = [[],[]];
+
+//richiesta al server
 $.getJSON( myURL, function(data) {
     console.log(data);
 
@@ -11,24 +13,57 @@ $.getJSON( myURL, function(data) {
     $.each(data, function(key, value) {
 
       //lista collegamenti url alle aziende
-      urls.push(value.url);
+      uri[0].push(value.nome);
+      uri[1].push(value.url);
 
       //lista di selezione in HTML
       items.push("<option value='" + key + "'>" + value.nome + "</option>");
     });
 
-    console.log(items);
     $('#selezione').append(items);
 });
-console.log(urls);
+console.log(uri);
 
-$('#selezione').change(
-  function() {
-  forEach(i: urls) {
-    if ($(this).val() === i) {
-      $('#info1').show();
-      $('#info2').show();
-    };
-  };
-}
-);
+$('#selezione').change(function() {
+
+  var id = $(this).val();
+  console.log(uri[id]);
+
+  if(id === 'def'){
+    $('#info1').hide();
+  }else{
+
+    //richiesta al server delle informazioni sull'azienda selezionata
+    $.getJSON(uri[1][id], function(data){
+        console.log(data);
+
+        //info
+        $('#iva').html("P.IVA: " + data.partitaIva);
+        $('#ragsoc').html("Ragione Sociale: "+ data.ragioneSociale);
+        $('#provincia').html("Provincia: " + data.provincia);
+        $('#disc').html("Data Iscrizione: " + data.dataIscrizione);
+        $('#dter').html("Data Termine: " + data.dataTermine);
+        $('#foro').html("Foro Competente: " + data.foroCompetente);
+        $('#rappresentante').html("Responsabile: " + data.cognomeRappresentante+" "+data.nomeRappresentante);
+        $('#responsabile').html("Rappresentante: " + data.cognomeResponsabile+" "+data.nomeResponsabile);
+
+        //contatti
+        $('#telR').html("Tel Responsabile: " + data.telResponsabile);
+        $('#email').html("Email: " + data.email);
+        $('#emailR').html("Email Responsabile: " + data.emailResponsabile);
+        $('#sede').html("Sede: " + data.indirizzoSede);
+        $('#citta').html("Città: " + data.citta);
+        $('#cap').html("Cap: " + data.cap);
+    });
+
+    //nome dell'azienda
+    $('#nomeAzienda').html(uri[0][id]);
+    $('#info1').show();
+  }
+
+
+
+
+
+
+});
